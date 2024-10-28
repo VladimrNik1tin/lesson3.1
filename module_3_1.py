cc60c248725a52054440fed4d08ec946cba080d8
VladimrNik1tin <nikitin.vladimir99@mail.ru>
2024-10-29 02:14:49 +0400

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,25 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+def is_contains(string, search_list):
+    count_calls()
+    string = string.lower()
+    for i in search_list:
+        if i.lower() == string:
+            return True
+    return False
+
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
